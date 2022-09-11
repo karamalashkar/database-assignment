ddf0a578ffa4b2254d08c746b9a0b0d1dced5da2
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-09-11 10:32:53 +0300

    universitydb-seventhquery

--- university-database-query7/universitydb-query7.sql
+++ university-database-query7/universitydb-query7.sql
@@ -1,3 +1,3 @@
-SELECT COUNT(DISTINCT(students.id)) 
-from students, enrolled, majorsln, departments 
-where students.id=enrolled.student_id and students.id = majorsln.students_id and majorsln.dept_name = departments.id and departments.name='CS';
+SELECT COUNT(DISTINCT(enrolled.student_id))
+from enrolled, majorsln, departments 
+where enrolled.student_id = majorsln.students_id and majorsln.dept_name = departments.id and departments.name='CS';
